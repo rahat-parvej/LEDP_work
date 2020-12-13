1e5e5488ac2d8b8e7bd68c64519d38a0b0eb4726
Rahat Parvej <53718164+rahat-parvej@users.noreply.github.com>
2020-12-13 10:01:46 +0600

    Add files via upload

---
+++ class12/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.first').owlCarousel({
+        loop:true,
+        margin:10,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:1500,
+        autoplayHoverPause:false,
+        fluidSpeed:true,
+        autoplaySpeed:false,
+        smartSpeed:250,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:2
+            }
+        }
+    });
+});
